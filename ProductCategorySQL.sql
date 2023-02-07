7419db3185c018aaaf48ae6d52416e1d091d7222
LandyshH <86878681+LandyshH@users.noreply.github.com>
2023-02-07 14:58:41 +0300

    Add SQL query

---
+++ ProductCategorySQL.sql
@@ -0,0 +1,5 @@
+SELECT Product.name, Category.name 
+FROM Product LEFT JOIN Product_Category 
+ON Product.id = Product_Category.product_id
+LEFT JOIN Category 
+ON Product_Category.category_id = Category.id
