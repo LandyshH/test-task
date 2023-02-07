9e098d54b9c0e3765b19036d5073384c8d0176ca
LandyshH <86878681+LandyshH@users.noreply.github.com>
2023-02-07 21:42:53 +0300

    Update ProductCategorySQL.sql

--- ProductCategorySQL.sql
+++ ProductCategorySQL.sql
@@ -1,5 +1,4 @@
 SELECT Product.name, Category.name 
-FROM Product LEFT JOIN Product_Category 
-ON Product.id = Product_Category.product_id
-LEFT JOIN Category 
-ON Product_Category.category_id = Category.id
+FROM Product 
+  LEFT JOIN Product_Category ON Product.id = Product_Category.product_id
+  LEFT JOIN Category ON Product_Category.category_id = Category.id
